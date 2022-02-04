265aa78858fd666a67cf159b8e20c01a8c74c15f
ryfleisch <ryfleisch@gmail.com>
2022-02-03 18:30:26 -0800

    fixed some formatting, adding shiba promo for the culture

--- webapp/model/web3helper.js
+++ webapp/model/web3helper.js
@@ -1,7 +1,17 @@
 /* refer to https://ethereum.stackexchange.com/questions/101858/how-to-get-the-erc-20-token-balance-of-an-account-using-etherjs
-/*const ethers = require('ethers');
+/*
+added this dependency
+const ethers = require('ethers');
+
+located in localservice/contract
 const genericErc20Abi = require(..../.../Erc20.json);
+
+refer to SupportedAssets.json
 const tokenContractAddress = '0x...';
+
+TODO figure this out for UI5
 const provider = ...; (use ethers.providers.InfuraProvider for a Node app or ethers.providers.Web3Provider(window.ethereum/window.web3) for a React app)
+
+TODO create helper methods for these
 const contract = new ethers.Contract(tokenContractAddress, genericErc20Abi, provider);
-const balance = (await contract.balanceOf((await provider.getSigners())[0].address)).toString();*
+const balance = (await contract.balanceOf((await provider.getSigners())[0].address)).toString();*/
